51310500ecb944a4900eaf57180bfd3d04b6b2a9
nguyenthanhsang18 <79785988+nguyenthanhsang18@users.noreply.github.com>
2021-05-12 16:54:45 +0700

    Add files via upload

---
+++ DTO/CBBitem.java
@@ -0,0 +1,23 @@
+package PBL3_DA.DTO;
+
+public class CBBitem {
+	private int Value;
+	private String Text;
+	public int GetValue()
+	{
+		return Value;
+	}
+	public void SetValue(int value)
+	{
+		this.Value = value;
+	}
+	public String GetText()
+	{
+		return Text;
+	}
+	public void SetText(String text)
+	{
+		this.Text = text;
+	}
+
+}
